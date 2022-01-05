82d470557a10ca89addd16c79abc24e9790e3348
damhuy03 <96248353+damhuy03@users.noreply.github.com>
2022-01-05 13:11:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    titleWeb: "Đàm Văn Huy",
+    introTitle: 'Hoàng Thị Anh Thư',
+    introDesc: `Và nếu em thấy cô đơn cô đơn
+    Anh sẽ luôn ở đây vì(iuu em)
+    Anh chẳng muốn công chúa của anh ướt mi`,
+    btnIntro: '^^heloo em iuu^^',
+    title: 'Phải chăng anh đã yêu ngay em từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy❤️ ',
+    btnYes: 'Yêu anh lắm lắm 💕💕<333',
+    btnNo: 'Dỗi anh không iu😑:3',
+    question: 'Trên thế giới hơn 7 tỉ người lý ro sao em lại yêu anh <333',
+    btnReply: 'Gửi cho anh người yêu <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu anh thuii tại vì anh là số 1 anh là siêu nhân 😂😂😂',
+    mess: 'Hì anh biết mà 🥰. Yêu em với cả tấm lòng 😘😘',
+    messDesc: 'Tối nay hát cho anh nghe nhé nhớ em hát quá.',
+    btnAccept: 'Okiiiii lun anhh<3',
+    messLink: 'https://www.facebook.com/profile.php?id=100043270928497' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
