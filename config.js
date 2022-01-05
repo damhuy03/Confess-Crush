6e884ea4c6dd3152886d1e29b8be7cbc505894ab
damhuy03 <96248353+damhuy03@users.noreply.github.com>
2022-01-05 13:21:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
     titleWeb: "Đàm Văn Huy",
     introTitle: 'Hoàng Thị Anh Thư',
-    introDesc: `Và nếu em thấy cô đơn cô đơn
-    Anh sẽ luôn ở đây vì(iuu em)
-    Anh chẳng muốn công chúa của anh ướt mi`,
-    btnIntro: '^^heloo em iuu^^',
-    title: 'Phải chăng anh đã yêu ngay em từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy❤️ ',
+    introDesc: `câu hỏi tối hôm nọ anh muốn hỏi nè`,
+    btnIntro: '^^chào em^^',
+    title: 'Phải chăng em đã yêu ngay anh từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy❤️ ',
     btnYes: 'Yêu anh lắm lắm 💕💕<333',
     btnNo: 'Dỗi anh không iu😑:3',
     question: 'Trên thế giới hơn 7 tỉ người lý ro sao em lại yêu anh <333',
     btnReply: 'Gửi cho anh người yêu <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu anh thuii tại vì anh là số 1 anh là siêu nhân 😂😂😂',
-    mess: 'Hì anh biết mà 🥰. Yêu em với cả tấm lòng 😘😘',
-    messDesc: 'Tối nay hát cho anh nghe nhé nhớ em hát quá.',
+    reply: 'Yêu thì yêu mà không yêu thì yêu😍',
+    reply: 'tại vì anh là số 1 anh là siêu nhân🤣🤣',
+    mess: 'Hì anh biết mà 🥰. Yêu em với cả trái tim này 😘😘',
+    messDesc: 'Tối nay hát cho anh nghe nhé nhớ em hát quá,giờ nhắn tin cho anh đi.',
     btnAccept: 'Okiiiii lun anhh<3',
     messLink: 'https://www.facebook.com/profile.php?id=100043270928497' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
